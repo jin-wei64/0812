ca701a4ea100e9ed52c1364186f09b057aa45c83
ryu <ryu64j@gmail.com>
2020-08-14 13:37:44 +0800

    ajax 01

--- Lab_Integrated/app.js
+++ Lab_Integrated/app.js
@@ -34,7 +34,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host : '127.0.0.1',
 	user : 'root',
-	password : '',
+	password : 'root',
+	port:"8889",
 	database : 'labdb'
 });
 
